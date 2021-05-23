17e3a11becb755938a8872dc7917861d4869bd75
ferzerkerx <ferzerkerx@gmail.com>
2021-05-23 20:55:10 +0200

    initial commit

--- src/utils/Utils.ts
+++ src/utils/Utils.ts
@@ -3,7 +3,7 @@ import * as crypto from 'crypto';
 export class Utils {
   static toHash(theText: string): string {
     return crypto
-      .createHash('sha256')
+      .createHash('sha512')
       .update(theText)
       .digest('hex');
   }
